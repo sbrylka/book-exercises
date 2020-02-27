ef536ce292800c360247919bc6694fbdb0a9cc1c
sbrylka <sbrylka96@gmail.com>
2020-02-27 19:02:15 +0100

    Chapter-06-04 exercises completed

--- chapter-06-exercises/exercise-4/exercise.R
+++ chapter-06-exercises/exercise-4/exercise.R
@@ -5,11 +5,20 @@
 # than or equal to twice the length of the other.
 # Hint: compare the length difference to the length of the smaller string
 
+is_twice_as_long <- function(string1, string2){
+  string1 <- nchar(string1)
+  string2 <- nchar(string2)
+  is_greater_or_equal <- string1 >= 2*string2 | string2 >= 2*string1
+  print(is_greater_or_equal)
+}
 
 # Call your `is_twice_as_long` function by passing it different length strings
 # to confirm that it works. Make sure to check when _either_ argument is twice
 # as long, as well as when neither are!
 
+is_twice_as_long("data","programming")
+is_twice_as_long("programming","data")
+is_twice_as_long("data","science")
 
 
 # Define a function `describe_difference` that takes in two strings. The
@@ -18,7 +27,21 @@
 #   "Your second string is longer by N characters"
 #   "Your strings are the same length!"
 
+describe_difference <- function(word1, word2){
+  diff <- nchar(word1) - nchar(word2)
+  if (diff > 0){
+    paste("Your first string is longer by", diff, "characters")
+  } else if (diff < 0){
+  paste("Your second string is longer by", -diff, "characters")
+} else {
+    print("Your strings are the same length!")
+  }
+}
+  
 
 # Call your `describe_difference` function by passing it different length strings
 # to confirm that it works. Make sure to check all 3 conditions1
 
+describe_difference("data","programming")
+describe_difference("programming","science")
+describe_difference("RStudio","science")
